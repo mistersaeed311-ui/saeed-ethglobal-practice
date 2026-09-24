11837b56d2e5b7a9a12525ae58c316f08b884cdb
Saeed <mistersaeed311@gmail.com>
2026-09-24 09:59:33 -0700

    Add learning progress text

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
       <p className="mt-4 text-xl">
         Building my first decentralized application
       </p>
+      <p className="mt-2 text-lg">
+  Learning React, Git and Web3
+</p>
 
       <button className="mt-8 px-6 py-3 rounded-xl bg-white text-black font-semibold">
         Connect Wallet

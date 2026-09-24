5db11081ac02c53ed5c09aa10a20183ee6ef5d6d
Saeed <mistersaeed311@gmail.com>
2026-09-24 11:34:30 -0700

    Add MetaMask wallet connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+"use client";
+import { useState } from "react";
 export default function Home() {
+  const [connected, setConnected] = useState(false);
+  const [account, setAccount] = useState("");
+  async function connectWallet() {
+  if (typeof window.ethereum !== "undefined") {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setAccount(accounts[0]);
+    setConnected(true);
+  } else {
+    alert("MetaMask not found!");
+  }
+}
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-5xl font-bold">
@@ -12,9 +28,17 @@ export default function Home() {
   Learning React, Git and Web3
 </p>
 
-      <button className="mt-8 px-6 py-3 rounded-xl bg-white text-black font-semibold">
-        Connect Wallet
-      </button>
+      <button
+ onClick={connectWallet}
+  className="mt-8 px-6 py-3 rounded-xl bg-white text-black font-semibold"
+>
+  {connected ? "Wallet Connected ✓" : "Connect Wallet"}
+</button>
+{account && (
+  <p className="mt-4">
+    Address: {account.slice(0, 6)}...{account.slice(-4)}
+  </p>
+)}
     </main>
   );
 }
